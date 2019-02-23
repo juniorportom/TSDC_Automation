3728e919737446e5ef1b99affee7c027cbdd8a2f
Reinaldo Portocarrero <juniorportom@gmail.com>
2019-02-23 18:04:04 -0500

    Se agrega logica de negocio para CRUD de aplicaciones

--- strategy/urls.py
+++ strategy/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from strategy.views.application import ApplicationCreate
+from strategy.views.application import ApplicationCreate, ApplicationList, ApplicationEdit, ApplicationDelete, ApplicationDetail
 
 urlpatterns = [
     path('create-application/', ApplicationCreate.as_view(), name='create-application'),
+    path('application-list/', ApplicationList.as_view(), name='application-list'),
+    path('edit-application/<int:pk>', ApplicationEdit.as_view(), name='edit-application'),
+    path('delete-application/<int:pk>', ApplicationDelete.as_view(), name='delete-application'),
+    path('detail-application/<int:pk>', ApplicationDetail.as_view(), name='detail-application'),
 ]
--- strategy/views/application.py
+++ strategy/views/application.py
@@ -1,16 +1,47 @@
 # coding=utf-8
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 from strategy.models.application import Application
 from strategy.forms.application import ApplicationForm
 
 
+@method_decorator(login_required(), name='dispatch')
 class ApplicationCreate(CreateView):
     model = Application
     form_class = ApplicationForm
     template_name = 'forms/application-form.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('application-list')
+
+
+@method_decorator(login_required(), name='dispatch')
+class ApplicationList(ListView):
+    model = Application
+    template_name = 'TSDC/application-list.html'
+    paginate_by = 20
+
+
+@method_decorator(login_required(), name='dispatch')
+class ApplicationEdit(UpdateView):
+    model = Application
+    form_class = ApplicationForm
+    template_name = 'forms/application-form.html'
+    success_url = reverse_lazy('application-list')
+
+
+@method_decorator(login_required(), name='dispatch')
+class ApplicationDetail(DetailView):
+    model = Application
+    template_name = 'TSDC/application-detail.html'
+
+
+@method_decorator(login_required(), name='dispatch')
+class ApplicationDelete(DeleteView):
+    model = Application
+    template_name = 'confirmation/delete-application.html'
+    success_url = reverse_lazy('application-list')
