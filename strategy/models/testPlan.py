a730f7dee1daddd177abf42c90199635d0bdc0e1
Reinaldo Portocarrero <juniorportom@gmail.com>
2019-03-08 15:19:28 -0500

    Se agrega status a la tabla de plan de pruebas

--- strategy/models/testPlan.py
+++ strategy/models/testPlan.py
@@ -7,6 +7,11 @@ from strategy.models.mobileOS import MobileSO
 
 
 class TestPlan(models.Model):
+    STATUS_TYPES = (
+        ('R', 'Registrado'),
+        ('P', 'Programado'),
+        ('E', 'Ejecutado')
+    )
     description = models.CharField(max_length=250)
     technique_test = models.ForeignKey(TechniqueTest, on_delete=models.CASCADE)
     browser = models.ForeignKey(Browser, on_delete=models.CASCADE, null=True)
@@ -15,6 +20,7 @@ class TestPlan(models.Model):
     execution_date = models.DateTimeField(default=datetime.now)
     create_at = models.DateTimeField(default=datetime.now)
     iterations = models.IntegerField(default=1)
+    status = models.CharField(max_length=1, choices=STATUS_TYPES, default='R')
     test_strategy = models.ForeignKey(TestStrategy, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -27,3 +33,7 @@ class TestPlan(models.Model):
 
     def script_name(self):
         return self.script_file.name.replace('scripts/', '')
+
+    def get_absolute_script_url(self):
+        # return 'https://s3.us-east-2.amazonaws.com/tsdc-automation.media/'+str(self.script_file.name)
+        return self.script_file.url
