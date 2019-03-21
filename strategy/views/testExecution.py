fca5f021e39749ed00d37396a1cc414e5f028725
Reinaldo Portocarrero <juniorportom@gmail.com>
2019-03-20 20:59:12 -0500

    Se corrigen filtros para que se muestren acorde al usuario en sesion

--- strategy/views/testExecution.py
+++ strategy/views/testExecution.py
@@ -8,6 +8,7 @@ from strategy.models.application import Application
 from strategy.models.applicationScript import ApplicationScript
 from strategy.models.testPlan import TestPlan
 from strategy.models.applicationType import ApplicationType
+from strategy.models.testStrategy import TestStrategy
 
 
 @method_decorator(login_required(), name='dispatch')
@@ -29,45 +30,38 @@ def executionFind(request):
     scripts = request.GET.getlist('scripts')
     status = request.GET.getlist('status')
 
-    applications = Application.objects.all()
-    executions = TestExecution.objects.all()
-    app_scripts = ApplicationScript.objects.all()
-    test_plans = TestPlan.objects.all()
-    app_types = ApplicationType.objects.all()
+    applications = Application.objects.filter(user=request.user)
+    executions = TestExecution.objects.filter(user=request.user)
+    app_scripts = ApplicationScript.objects.filter(application__in=applications)
 
-    sts = []
-    for st in status:
-        sts.append(st)
-        executions = executions.filter(status__in=sts)
+    if status:
+        executions = executions.filter(status__in=status)
 
-    for pk in types:
-        app_types = app_types.filter(id__in=pk)
-        applications = applications.filter(type__in=app_types)
+    if types:
+        applications = applications.filter(type__in=types)
         app_scripts = app_scripts.filter(application__in=applications)
         executions = executions.filter(script__in=app_scripts)
 
-    for pk in scripts:
-        app_scripts = app_scripts.filter(id__in=pk)
+    if scripts:
+        executions = executions.filter(script__in=scripts)
+
+    if plans:
+        executions = executions.filter(test_plan__in=plans)
+
+    if apps:
+        app_scripts = app_scripts.filter(application__in=apps)
         executions = executions.filter(script__in=app_scripts)
 
-    for pk in plans:
-        test_plans = test_plans.filter(id__in=pk)
-        executions = executions.filter(test_plan__in=test_plans)
-
-    for pk in apps:
-        applications = applications.filter(id=pk)
-        app_scripts = app_scripts.filter(application__in=applications)
-        executions = executions.filter(script__in=app_scripts)
-
-    all_apps = Application.objects.all()
-    all_plans = TestPlan.objects.all()
+    all_apps = Application.objects.filter(user=request.user)
+    all_strategies = TestStrategy.objects.filter(user=request.user)
+    all_plans = TestPlan.objects.filter(test_strategy__in=all_strategies)
     all_types = ApplicationType.objects.all()
-    all_scripts = ApplicationScript.objects.all()
+    all_scripts = ApplicationScript.objects.filter(application__in=all_apps)
     all_status = TestExecution.STATUS_TYPES
 
     context = {
         'searchParams': {
-            'isSearch': len(apps) != 0 or len(plans) !=0,
+            'isSearch': len(apps) != 0 or len(plans) != 0 or len(types) != 0 or len(scripts) != 0 or len(status) != 0,
             'plans': plans,
             'apps': apps,
             'types': types,
