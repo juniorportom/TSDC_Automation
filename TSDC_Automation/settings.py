44ac448a958c50dcaf0c0f81a2ed07d1ebe3ac7c
Reinaldo Portocarrero <juniorportom@gmail.com>
2019-03-24 20:29:33 -0500

    Se despliegan recursos por mediante cloudfront, ademas se corrigen algunos errores en titulos

--- TSDC_Automation/settings.py
+++ TSDC_Automation/settings.py
@@ -35,10 +35,10 @@ AWS_SECRET_ACCESS_KEY_SQS = os.environ.get('AWS_SECRET_ACCESS_KEY_SQS')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL')
-AWS_S3_OBJECT_PARAMETERS = {
-    'ContentType': 'application/force-download',
-    'ContentDisposition': 'attachment'
-}
+# AWS_S3_OBJECT_PARAMETERS = {
+#    'ContentType': 'application/force-download',
+#    'ContentDisposition': 'attachment'
+# }
 
 
 # Application definition
